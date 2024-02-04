9494d68fc1011005a82e7d5f44b992c53ee95de0
Cairo A <cairohyfr@gmail.com>
2024-02-03 20:11:55 -0600

    another heorku fix pt. idk at this point this annoying af

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 from flask_restx import Api
 from flask_jwt_extended import JWTManager
@@ -10,12 +10,11 @@ from extensions import db, migrate  # Assuming you've moved db and migrate to ex
 load_dotenv()
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='build', static_url_path='')
     CORS(app)
 
     api = Api(app, version='1.0', title='My API', description='A Simple API', doc='/docs')
 
-   
     database_url = os.getenv('DATABASE_URL', 'sqlite:///yourdefault.db')
     if database_url.startswith("postgres://"):
         database_url = database_url.replace("postgres://", "postgresql://", 1)
@@ -23,20 +22,21 @@ def create_app():
     
     app.config['JWT_SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-    
     db.init_app(app)
     migrate.init_app(app, db)
     JWTManager(app)
 
-  
     from routes.users import api as users_api
     api.add_namespace(users_api, path='/api/users')
-   
-   
 
-    @app.route('/')
-    def hello():
-        return "Hello, World!"
+    
+    @app.route('/', defaults={'path': ''})
+    @app.route('/<path:path>')
+    def serve(path):
+        if path != "" and os.path.exists(app.static_folder + '/' + path):
+            return send_from_directory(app.static_folder, path)
+        else:
+            return send_from_directory(app.static_folder, 'index.html')
 
     return app
 
