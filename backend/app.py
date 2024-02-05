922fd1113c7d5977acfd305b6ec9448cbee1d5c2
Cairo A <cairohyfr@gmail.com>
2024-02-04 21:42:16 -0600

    idk

--- backend/app.py
+++ backend/app.py
@@ -1,36 +1,3 @@
-from flask import Flask, send_from_directory
-from flask_cors import CORS
-from flask_restx import Api
-from flask_jwt_extended import JWTManager
-from dotenv import load_dotenv
-import os
-
-from extensions import db, migrate  
-
-load_dotenv()
-
-def create_app():
-    app = Flask(__name__)
-    CORS(app)
-
-    api = Api(app, version='1.0', title='My API', description='A Simple API', doc='/docs')
-
-    database_url = os.getenv('DATABASE_URL', 'sqlite:///yourdefault.db')
-    if database_url.startswith("postgres://"):
-        database_url = database_url.replace("postgres://", "postgresql://", 1)
-    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
-    
-    app.config['JWT_SECRET_KEY'] = os.getenv('SECRET_KEY')
-
-    db.init_app(app)
-    migrate.init_app(app, db)
-    JWTManager(app)
-
-    from routes.users import api as users_api
-    api.add_namespace(users_api, path='/api/users')
-
-    return app  
-
-if __name__ == '__main__':
-    app = create_app()  
-    app.run(host='localhost') 
+version https://git-lfs.github.com/spec/v1
+oid sha256:812a5409802d35720877ab17bbc8ff20b834ff3b4babd7d4398e745310765baf
+size 1018
