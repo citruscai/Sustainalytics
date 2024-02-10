040cd007f38303101d663cb91f393fb7a036bd03
Cairo A <cairohyfr@gmail.com>
2024-02-09 19:42:23 -0600

    update landing

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import UnicornAnimation from '../images/UnicornAnimation.gif';
+
 
 const LandingPage: React.FC = () => {
     return (
         <div className="text-center mt-12">
-            <h1 className="text-4xl font-bold">Welcome to My React App</h1>
-            <p className="mt-4 text-lg">This is a simple landing page built with React, TypeScript, and Tailwind CSS.</p>
+            <h1 className="text-4xl font-bold">Welcome</h1>
+            <div className="flex justify-center items-center">
+            <img src={UnicornAnimation} alt="loading..." width={500}height={500}/>
+            </div>
+           
         </div>
     );
 };
